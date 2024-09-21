f35c67916aa58d07dc75162d84d92ca1a8cb5ac7
codenamehenny <genesis09m@hotmail.com>
2024-09-20 21:49:59 -0400

    book operations added and adjusted main system to import book_ops

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# main library system
+import book_ops
+
 def main_system():
     while True:
         try:
@@ -5,8 +8,7 @@ def main_system():
             print("1. Book Operations\n2. User Operations\n3. Author Operations \n4. Quit")
             main_menu_option = int(input("Enter option number here: "))
             if main_menu_option == 1:
-                print("Book Operations:\n1. Add a new book\n2. Borrow a book\n3. Return a book")
-                print("4. Search for a book\n5. Display all books\n6. Back to Main Menu")
+                book_ops.book_ops_menu()
             elif main_menu_option ==  2:
                 print("User Operations:\n1. Add a new user\n2. View user details\n3. Display all users\n4. Back to Main Menu")
             elif main_menu_option == 3:
@@ -18,7 +20,10 @@ def main_system():
                 print("\nEnter a valid number between 1-4")
         except ValueError:
             print("Please Enter a valid number")
-        except Exception as e:
-            print(f"Error message: '{e}'. Please try again")
+        #except Exception as e:
+        #    print(f"Error message: '{e}'. Please try again")
+        finally:
+            print("Thanks for using the library management system!")
 
-main_system()
+if __name__ == "__main__":
+    main_system()
